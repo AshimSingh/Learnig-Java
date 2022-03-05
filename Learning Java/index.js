468d12f6ad8a25087904ca2052bb61c594ba0155
AshimSingh <ashimsingh948@gmail.com>
2022-03-05 10:05:21 +0545

    learnig constructors

--- Learning Java/index.js
+++ Learning Java/index.js
@@ -10,8 +10,8 @@ console.log(name+lastname);
 document.getElementById("demo").innerHTML=name;
 function myFunction(){
     if(name==="Ashim"){
-        alert("you must be older than age");
-        alert(adon);
+        alert("you must be older than "+adon+" years");
+        
         name="Daddy";
         document.getElementById('demo').innerHTML=name;
     }
@@ -20,3 +20,42 @@ function myFunction(){
         document.getElementById('demo').innerHTML=name;
     }
 }
+const figure ={
+    radius:8,
+    location:"Kathmandu",
+    monalisa:{
+        artist:"Leando da vince",
+        date:1887
+    },
+    draw:function head(){
+        console.log("draw two eyes two ears");
+    }
+}
+figure.draw();
+console.log(figure.monalisa.artist);
+//console.log(figure["radius"]);
+//Facotry Function
+    //Factory function is a function that returns methods or property. First it creats the object and returns it.
+function createCircle(radius){
+    return {
+        radius,
+        draw:function(){
+            console.log("Circle created of radius "+radius);
+        }
+    }
+}
+const Circle= createCircle(1);
+console.log(Circle.radius);
+const bigCircle= createCircle(100);
+bigCircle.draw();
+//Constructor Function
+function Car(Disel){
+    this.Disel=Disel;
+    this.wheel= "Milestone";
+    this.start=function(){
+        alert("Car has started");
+    }
+};
+let tesla= new Car("no disel");
+console.log(tesla.wheel);
+tesla.start();
